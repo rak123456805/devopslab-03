7e8c1c4cd730dffa7795d90e65831cde0ba69ecd
rak123456805 <hnrakshith4@gmail.com>
2026-03-17 10:46:26 +0530

    initial commit

---
+++ src/test/java/WebPageTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+public class WebPageTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://rak123456805.github.io/DEVOPS-lab/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        System.out.println("Page Title: " + actualTitle);
+
+        String expectedTitle = "My Simple Website";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
+        Assert.assertTrue(actualTitle.contains("Website"));
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
